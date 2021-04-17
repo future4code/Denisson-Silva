d25f95efc717356ce476cc51ba7bd78d92342c26
Denisson-Silva <denisson.boulevardmotos@gmail.com>
2021-04-17 20:20:50 -0300

    exercicio 4

--- quinzena4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
+++ quinzena4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
@@ -35,8 +35,15 @@ function retornaNumerosPares (array) {
 //Exercício 4
 
 function retornaMaiorNumero(array) {
-   // implemente sua lógica aqui
+   array [0, 8, 23, 16, 10, 15, 41, 12, 13]
+   let maiorNumero = 0
+   for(let i = 0; i < array.length; i++){
+      if(array[i] > maiorNumero){
+         maiorNumero = array[i]
+      } 
+   } return maiorNumero
 }
+
 
 //Exercício 5
 
