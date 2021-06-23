29f0723201539b4d5722e3d8b30a3b11cc6f6693
Denisson-Silva <denisson.boulevardmotos@gmail.com>
2021-06-22 23:19:50 -0300

    Inicio do projeto

---
+++ quinzena9/projeto/labex/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import AccessAlarmsIcon from '@material-ui/icons/AccessAlarms';
+
+
+
+function App() {
+  return (
+    <div >
+      LabeX
+      <AccessAlarmsIcon/>
+    </div>
+  );
+}
+
+export default App;
